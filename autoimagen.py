94d28f65a78875f96f57f2b9f12fcc96ccfa18f5
Chicibelito70 <Chicibelito07@gmail.com>
2022-11-24 14:39:23 -0400

    Version 1

---
+++ autoimagen.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+
+downloadsFolder = "/Users/Chici/"
+picturesFolder = "/Users/Chici/Pictures/"
+
+if __name__ == "__main__":
+    for filename in os.listdir(downloadsFolder):
+        name, extension = os.path.splitext(downloadsFolder + filename)
+
+        if extension in [".jpg", ".jpeg", ".png"]:
+            picture = Image.open(downloadsFolder + filename)
+            picture.save(picturesFolder + "compressed_"+filename, optimize=True, quality=60)
+            os.remove(downloadsFolder + filename)
+            print(name + ": " + extension)
+
+        if extension in [".mp3"]:
+            musicFolder = "/Users/Chici/Music/"
+            os.rename(downloadsFolder + filename, musicFolder + filename)
+
+
